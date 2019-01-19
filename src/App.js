1c469cfa705b4ea0d2cd4f9aeea9ef5d52b29a00
Jack Yeh <jckyeh@gmail.com>
2019-01-19 20:08:51 +0000

    added ImageLinkForm, Rank, Particles components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
+const particlesOptions = {
+  "particles": {
+      "number": {
+          "value": 200
+      },
+      "size": {
+          "value": 3
+      }
+  },
+  "interactivity": {
+      "events": {
+          "onhover": {
+              "enable": true,
+              "mode": "repulse"
+          }
+      }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles 
+          className="particles"
+          params={particlesOptions} />
         <Navigation />
         <Logo />
-        {/* <ImageLinkForm />
-        <FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ImageLinkForm.css'
+
+const ImageLinkForm = () => {
+    return (
+        <div>
+            <p className="f3">{'This Magic Brain will detect faces in images. Give it a try!'}</p>
+            <div className="center">
+                <div className="pa4 br3 shadow-5 center form">
+                    <input type="text" className="f4 pa2 w-70 center" />
+                    <button className="w-30 f4 grow ph3 pv2 white bg-light-purple link dib">Enter</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
