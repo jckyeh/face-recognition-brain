80fd064068600aeaf9df87f82bb62e160c0aad9e
Jack Yeh <jckyeh@gmail.com>
2019-01-20 16:02:58 +0000

    Added FaceRecognition component, started with Clarifai API

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+    return (
+        <div className="center">
+            <img 
+                src={'https://samples.clarifai.com/face-det.jpg'}
+                alt='test face' 
+            />
+        </div>
+    );
+}
+
+export default FaceRecognition;
