ce150cdd8f20aaf8f1c8cc514c45904a348b8990
Jack Yeh <jckyeh@gmail.com>
2019-01-22 16:17:07 +0000

    Added calculateFaceLocation and displayFaceBox functions, got Face Recognition model working

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageURL }) => {
+const FaceRecognition = ({ box, imageURL }) => {
     return (
         <div className="center ma">
             <div className="absolute mt2">
                 <img 
+                    id = 'inputImage'
                     width="500px"
                     height='auto'
                     src={imageURL}
                     alt='test face' 
                 />
+                <div 
+                    style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}
+                    className='bounding-box'></div>
             </div>
         </div>
     );
